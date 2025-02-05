acc14a395f5b28ad3c438c8b437a21bc4eaa4f2b
Courts02 <courtneycook2006@gmail.com>
2025-02-05 09:37:40 +0200

    publish chapters 3 and 4

---
+++ Chapter 4/main.js
@@ -0,0 +1,70 @@
+var myPerson = {
+    "name": "Courtney",
+    "age": 18,
+    "favoriteColor": "Green"
+   };
+
+var myWholeFamily = [
+{
+   "relation": "Mom",
+   "name": "Devanae",
+   "age": 40 ,
+},
+{
+    "relation": "Boyfriend",
+    "name": "Daniel",
+    "age": 32,
+},
+{
+    "relation": "Best Friend",
+    "name": "Tristen",
+    "age": 19,
+},
+{
+    "relation": "Cousin",
+    "name": "Ben",
+    "age": 18 ,
+},
+{
+    "relation": "Cousin",
+    "name": "Leah",
+    "age": 17,
+}
+];
+
+var videoGames = {
+    "Valorant": { 
+        "releaseDate": 2020,
+        "genre": "tacticalShooter",
+    },
+        "Call Of duty: Warzone": {
+            "releaseDate":  2020,
+            "genre": "fisrtPersonShooter",
+    },
+    "Genshin Impact":{ 
+        "releaseDate":  2020,
+        "genre": "actionrole-Playing",
+    },
+};
+
+var movies = {
+    "Finding Nemo": {
+    releaseDate: 2003,
+    duration: 100,
+    actors: ["Albert Brooks", "Ellen DeGeneres", "Alexander Gould"],
+    format: "DVD"
+    },
+    "Star Wars: Episode VI - Return of the Jedi": {
+    releaseDate: 1983,
+    duration: 134,
+    actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+    format: "DVD"
+    },
+    "Harry Potter and the Goblet of Fire": {
+    releaseDate: 2005,
+    duration: 157,
+    actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+    format: "Blu-ray"
+    }
+   };
+   
