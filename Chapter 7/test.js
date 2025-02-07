eb89d9f89ddab3fd3068ad55908ce83a1e60f49f
Courts02 <courtneycook2006@gmail.com>
2025-02-07 08:41:34 +0200

    Chapter 7 & 8

---
+++ Chapter 7/test.js
@@ -0,0 +1,20 @@
+var colour = prompt("What is your favorite colour?");
+var js = confirm("Do you like JavaScript?");
+
+alert("Boom'");
+
+console.log(colour + " is a nice colour.");
+
+if (js) {
+    console.log("That is cool.")
+} else {
+    console.log("Yeah...Me neither.")
+};
+
+var testVariable = prompt("Insert something");
+
+if (testVariable === 1) {
+    alert("That is true.");
+} else {
+    alert("That is false.")
+};
---
+++ Chapter 8/Main.js
@@ -0,0 +1,34 @@
+// var sayHello = function (myName) {
+//     console.log("Hello there, " + myName + " How are you?");
+//    };
+
+//    sayHello("Courts.");
+
+//    var drawCats = function (howManyTimes) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//     console.log(i + " =^.^=");
+//     }
+//    };
+
+//    drawCats(5);
+
+//    var printMultipleTimes = function (howManyTimes, whatToDraw) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//     console.log(i + " " + whatToDraw);
+//     }
+
+//    };
+//    function() {
+//       console.log()
+//    }
+
+//    var doubles = function(Number) {
+//       return Number *= 2
+//    }
+//    doubles(5) + double(6)
+
+var doubles = function (number) {
+   var dNumber = number *= 2
+   return dNumber
+}
+
